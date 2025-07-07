71d602d2d3e27e05c95c1e9c61baeb26a0f986b7
Corentin Fugier <corentinfugier@hotmail.com>
2025-07-07 13:51:01 +0200

    hardcoded jwt key for dev

--- customer-service/app/config.py
+++ customer-service/app/config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "b7e2e2e1e7e6c1a2b3d4f5c6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
